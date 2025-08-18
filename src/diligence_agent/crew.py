95513f92bf320f22666341a133c40a060f8ac060
George Bezerra <gbezerra@gmail.com>
2025-08-18 10:41:28 -0600

    Initial commit

---
+++ src/diligence_agent/crew.py
@@ -0,0 +1,57 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+from crewai_tools import SerperDevTool
+
+
+@CrewBase
+class DiligenceAgent():
+    """DiligenceAgent crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher'], # type: ignore[index]
+            verbose=True,
+            llm="gpt-4o-mini",
+            tools=[SerperDevTool()]
+        )
+
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            verbose=True,
+            llm="gpt-4o-mini"
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'], # type: ignore[index]
+            llm="gpt-4o-mini"  # Set specific model for this task
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            output_file='report.md',
+            llm="gpt-4o-mini"
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the DiligenceAgent crew"""
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
---
+++ src/diligence_agent/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from diligence_agent.crew import DiligenceAgent
+
+from opik.integrations.crewai import track_crewai
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+
+track_crewai(project_name="diligence-agent")
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs',
+        'current_year': str(datetime.now().year)
+    }
+    
+    try:
+        DiligenceAgent().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        'current_year': str(datetime.now().year)
+    }
+    try:
+        DiligenceAgent().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        DiligenceAgent().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    
+    try:
+        DiligenceAgent().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
